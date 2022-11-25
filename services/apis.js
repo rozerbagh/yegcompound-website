c6fad1dab65fbf1343c15f44d3919fb99476fa25
rozerbagh <rozerbagh456@gmail.com>
2022-11-25 18:42:12 +0530

    the base url has been changed

--- services/apis.js
+++ services/apis.js
@@ -1,4 +1,5 @@
-const baseURL = 'http://3.110.157.141:3003/api/v1'
+// const baseURL = 'http://3.110.157.141:3003/api/v1'
+const baseURL = 'https://api.rozerbagh.com/api/v1'
 // const baseURL = 'http://localhost:3003/api/v1'
 export const login = `${baseURL}/user/login`;
 export const signup = `${baseURL}/user/signup`;
