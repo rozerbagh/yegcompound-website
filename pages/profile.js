0e19f52a072a61fbbe404ae7122fb7e49660b0a3
rozerAdda247 <bag.rozer@adda247.com>
2023-10-02 05:16:43 +0530

    the buil

---
+++ pages/profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <section className="profile-sections w-100 h-100  banner2 p-t-30">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="contianer">
+        <div className="row">
+          <div className="col-12 d-flex flex-row justify-content-center align-items-center p-12">
+            <h1 className="fs-1">Profile Section Comming Soon</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Profile;
